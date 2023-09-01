a6f2af7d3af163cd4357aa26f17fe513129cc295
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-01 22:11:59 +0200

    primjer

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -14,19 +14,7 @@ import javafx.stage.Stage;
 public class AppFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-       /* Scene scene = new Scene(new Button("OK"), 200, 250);
-        primaryStage.setTitle("MyJavaFX"); // Set the stage title
-        primaryStage.setScene(scene); // Place the scene in the stage
-        primaryStage.show(); // Display the stage
-        Stage stage = new Stage(); // Create a new stage
-        stage.setTitle("Second Stage"); // Set the stage title
-        // Set a scene with a button in the stage
-        stage.setScene(new Scene(new Button("New Stage"), 100, 100));
-        stage.show(); // Display the stage*/
-
-
-        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/home.fxml"));
         primaryStage.setTitle("Hello world");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
