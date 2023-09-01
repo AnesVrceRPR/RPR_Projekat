f6919a2a0943c450627e1f9f4f52dabd6e4fe45f
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-01 18:48:45 +0200

    delete

--- src/main/java/ba/unsa/etf/rpr/dao/UcesnikDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UcesnikDaoSQLImpl.java
@@ -77,4 +77,15 @@ public class UcesnikDaoSQLImpl implements UcesnikDao {
         }
     }
 
+    public void delete(int id) {
+        String delete = "DELETE FROM Ucesnik WHERE id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(delete, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
