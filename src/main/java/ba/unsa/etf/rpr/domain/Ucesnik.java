c7662abc2ba470cc2f3a1d41c1ac832f685546e5
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-01 18:40:16 +0200

    ispravka

--- src/main/java/ba/unsa/etf/rpr/domain/Ucesnik.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Ucesnik.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.domain;
 
 public class Ucesnik {
+    private int id;
+    private String Ime_i_Prezime;
+    private int Rejting;
+    private int Broj_Osvojenih_Bodova;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {this.id = id;}
+
+    public String getIme_i_Prezime() {
+        return Ime_i_Prezime;
+    }
+
+    public void setIme_i_Prezime(String Ime_i_Prezime) {
+        this.Ime_i_Prezime = Ime_i_Prezime;
+    }
+
+    public int getRejting() {
+        return Rejting;
+    }
+
+    public void setRejting(int rejting) {
+        Rejting = rejting;
+    }
+
 }
