e88f78a2e4ecbf5846b228c00abc07a05a21f3af
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-01 18:25:58 +0200

    get/set id

--- src/main/java/ba/unsa/etf/rpr/dao/UcesnikDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UcesnikDaoSQLImpl.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.dao;
 
-public class UcesnikDaoSQLImpl {
+public class UcesnikDaoSQLImpl{
+    private int id;
+    private String Ime_i_Prezime;
+    private int Rejting;
+    private int Broj_Osvojenih_Bodova;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {this.id = id;}
 }
