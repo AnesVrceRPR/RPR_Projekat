6cc8bde3ce76264418d17df72c9f91c1448b14a4
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 12:39:53 +0200

    get/set igrac1

--- src/main/java/ba/unsa/etf/rpr/domain/SljedeceKolo.java
+++ src/main/java/ba/unsa/etf/rpr/domain/SljedeceKolo.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.domain;
 
 public class SljedeceKolo {
+    String igrac1;
+    String igrac2;
+
+    public String getIgrac1() {
+        return igrac1;
+    }
+
+    public void setIgrac1(String igrac1) {
+        this.igrac1 = igrac1;
+    }
+
+
 }
