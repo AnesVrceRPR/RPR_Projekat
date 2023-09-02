19ce7570add456d5d4c426dda6c85fe999ae23c4
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 03:14:41 +0200

    Dodana metoda dodajIgraca u NoviKontroler

--- src/main/java/ba/unsa/etf/rpr/controller/NoviKontroler.java
+++ src/main/java/ba/unsa/etf/rpr/controller/NoviKontroler.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr.controller;
 
-public class NoviKontroler {
+import ba.unsa.etf.rpr.dao.UcesnikDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Ucesnik;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class NoviKontroler implements Initializable {
+    public ListView ListaImena;
+
+    public Button PotvrdiDodavanje;
+    public TextField label1;
+    public TextField label2;
+    public Button potvrdiBrisanje;
+    public TextField label3;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
+        ListaImena.getItems().addAll(ucesnikDaoSQL.getAll());
+    }
+
+    public void dodajIgraca(ActionEvent actionEvent) {
+        Ucesnik ucesnik = new Ucesnik();
+        ucesnik.setIme_i_Prezime(label1.getText());
+        ucesnik.setRejting(Integer.parseInt(label2.getText()));
+
+        UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
+        ucesnikDaoSQL.add(ucesnik);
+
+        ListaImena.getItems().add(ucesnik);
+    }
+
+
 }
