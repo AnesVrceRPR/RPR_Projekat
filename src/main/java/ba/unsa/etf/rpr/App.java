d8debab1811ac137b8110a252e4049b0547c6260
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 03:12:11 +0200

    testiranje getAll metode

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -3,6 +3,8 @@ package ba.unsa.etf.rpr;
 import ba.unsa.etf.rpr.dao.UcesnikDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Ucesnik;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -11,14 +13,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        Ucesnik ucesnik = new Ucesnik();
+        /*Ucesnik ucesnik = new Ucesnik();
         ucesnik.setIme_i_Prezime("anes");
         ucesnik.setId(3);
         ucesnik.setRejting(1900);
         ucesnik.setBroj_Osvojenih_Bodova(0);
 
         UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
-        ucesnikDaoSQL.update(ucesnik);
+        ucesnikDaoSQL.update(ucesnik);*/
+
+        UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
+        List<Ucesnik> lista = ucesnikDaoSQL.getAll();
+
+        for(Ucesnik ucesnik : lista)
+        {
+            System.out.println(ucesnik.getIme_i_Prezime());
+        }
 
     }
 }
