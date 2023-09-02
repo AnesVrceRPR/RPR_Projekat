8b98840b9a667b657843e1bee5578263602583c4
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 13:17:46 +0200

    connection

--- src/main/java/ba/unsa/etf/rpr/dao/TabelaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TabelaDaoSQLImpl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
-public class TabelaDaoSQLImpl {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TabelaDaoSQLImpl implements TabelaDao {
+    private Connection connection;
+
+    public TabelaDaoSQLImpl(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_RPRbaza1","freedb_avrce1", "sUf2N#3?An58!@h");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 }
