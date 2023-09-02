d1bd5823943511c63ac646ebc7e5be10014620c3
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 13:21:30 +0200

    getteri za sva mjesta

--- src/main/java/ba/unsa/etf/rpr/dao/TabelaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TabelaDaoSQLImpl.java
@@ -14,9 +14,4 @@ public class TabelaDaoSQLImpl implements TabelaDao {
         }
     }
 
-
-
-
-
-
 }
---
+++ src/main/java/ba/unsa/etf/rpr/domain/Tabela.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Tabela {
+    private int id;
+    private String mjesto1;
+    private String mjesto2;
+    private String mjesto3;
+    private String mjesto4;
+    private String mjesto5;
+    private String mjesto6;
+    private String mjesto7;
+    private String mjesto8;
+
+    public String getMjesto1() {
+        return mjesto1;
+    }
+
+    public String getMjesto2() {
+        return mjesto2;
+    }
+
+    public String getMjesto3() {
+        return mjesto3;
+    }
+
+    public String getMjesto4() {
+        return mjesto4;
+    }
+
+    public String getMjesto5() {
+        return mjesto5;
+    }
+
+    public String getMjesto6() {
+        return mjesto6;
+    }
+
+    public String getMjesto7() {
+        return mjesto7;
+    }
+
+    public String getMjesto8() {
+        return mjesto8;
+    }
+
+
+}
