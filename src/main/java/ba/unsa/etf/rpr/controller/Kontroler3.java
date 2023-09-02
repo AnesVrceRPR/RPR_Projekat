5cb8080585744a090a0ef4b3774869778a5099ac
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-02 13:16:45 +0200

    TablelaDao

---
+++ src/main/java/ba/unsa/etf/rpr/controller/Kontroler3.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Kontroler3 implements Initializable {
+    public Label igrac3;
+    public Label igrac1;
+    public Label igrac5;
+    public Label igrac7;
+    public Label igrac2;
+    public Label igrac4;
+    public Label igrac6;
+    public Label igrac8;
+    public ListView tabela;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
