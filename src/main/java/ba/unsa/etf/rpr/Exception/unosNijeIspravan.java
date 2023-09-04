fe9d69f1f8dd366afd6f31d32cd78a0b77a174c2
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-05 01:06:44 +0200

    OdigranaKolaMenager javadoc

--- src/main/java/ba/unsa/etf/rpr/Exception/unosNijeIspravan.java
+++ src/main/java/ba/unsa/etf/rpr/Exception/unosNijeIspravan.java
@@ -1,5 +1,9 @@
 package ba.unsa.etf.rpr.Exception;
 
+/**
+ * Korisnicki definisan Exception
+ */
+
 public class unosNijeIspravan extends RuntimeException{
 
     public unosNijeIspravan(String poruka){
--- src/main/java/ba/unsa/etf/rpr/business/OdigranaKolaMenager.java
+++ src/main/java/ba/unsa/etf/rpr/business/OdigranaKolaMenager.java
@@ -9,8 +9,18 @@ import ba.unsa.etf.rpr.domain.Tabela;
 
 import java.util.List;
 
+/**
+ * Business Logic Layer za upravljanje odigranim kolima
+ */
+
 public class OdigranaKolaMenager {
 
+    /**
+     * metoda vraća odigrano kolo iz baze sa odgovarajućim id
+     * baca izuzetak ako kolo ne postoji
+     * @param id - id od željenog kola
+     * @return OdigranaKola
+     */
     public OdigranaKola getById(int id)
     {
         OdigranaKolaDaoSQLImpl odigranaKolaDaoSQL = new OdigranaKolaDaoSQLImpl();
@@ -28,6 +38,11 @@ public class OdigranaKolaMenager {
         return odigranaKolaDaoSQL.getById(id);
     }
 
+    /**
+     * metoda briše odgovarajuće odigrano kolo iz baze
+     * baca izuzetak ako kolo ne postoji
+     * @param id - id od kola koje se želi obrisati
+     */
     public void delete(int id)
     {
         OdigranaKolaDaoSQLImpl odigranaKolaDaoSQL = new OdigranaKolaDaoSQLImpl();
