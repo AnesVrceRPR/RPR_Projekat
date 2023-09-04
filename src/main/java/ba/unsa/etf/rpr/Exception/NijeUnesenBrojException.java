bf3a7c6aa0d7d6c44d0db5695c34418241024bad
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-04 17:10:07 +0200

    exception

---
+++ src/main/java/ba/unsa/etf/rpr/Exception/NijeUnesenBrojException.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.Exception;
+
+public class NijeUnesenBrojException extends RuntimeException{
+
+    public NijeUnesenBrojException(String poruka){
+        super(poruka);
+    }
+}
