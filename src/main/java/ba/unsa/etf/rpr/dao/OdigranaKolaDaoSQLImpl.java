15b728ccf65b5740f6c40f226e22698bdb10e213
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-04 14:58:26 +0200

    connection

--- src/main/java/ba/unsa/etf/rpr/dao/OdigranaKolaDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OdigranaKolaDaoSQLImpl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
-public class OdigranaKolaDaoSQLImpl {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class OdigranaKolaDaoSQLImpl implements OdigranaKolaDao {
+
+    private Connection connection;
+
+    public OdigranaKolaDaoSQLImpl(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_RPRbaza1","freedb_avrce1", "sUf2N#3?An58!@h");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 }
