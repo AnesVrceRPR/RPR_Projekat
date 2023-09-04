e8ab2f86f411b5cdeb8999a456aa9b13ed77c756
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-04 21:06:21 +0200

    implementacija TabelaMenager getByID

--- src/main/java/ba/unsa/etf/rpr/business/TabelaMenager.java
+++ src/main/java/ba/unsa/etf/rpr/business/TabelaMenager.java
@@ -1,12 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Exception.unosNijeIspravan;
+import ba.unsa.etf.rpr.dao.TabelaDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.UcesnikDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Tabela;
+import ba.unsa.etf.rpr.domain.Ucesnik;
+
+import java.util.List;
 
 public class TabelaMenager {
 
     public Tabela getById(int id)
     {
+        TabelaDaoSQLImpl tabelaDaoSQL = new TabelaDaoSQLImpl();
+        List<Tabela> tabele = tabelaDaoSQL.getAll();
 
+        boolean brojac = false;
+
+        for(Tabela tabela: tabele)
+        {
+            if(tabela.getId() == id) brojac = true;
+        }
+
+        if(brojac == false) throw new unosNijeIspravan("Tabela ne postoji");
+
+        return tabelaDaoSQL.getById(id);
+    }
 
 
 
