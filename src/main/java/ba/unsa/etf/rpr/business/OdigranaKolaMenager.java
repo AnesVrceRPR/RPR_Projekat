dcd49568ba9d51361d052f95a1a157875689d83a
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-04 21:27:05 +0200

    implementacija metode delete

--- src/main/java/ba/unsa/etf/rpr/business/OdigranaKolaMenager.java
+++ src/main/java/ba/unsa/etf/rpr/business/OdigranaKolaMenager.java
@@ -1,10 +1,31 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.Exception.unosNijeIspravan;
+import ba.unsa.etf.rpr.dao.OdigranaKolaDao;
+import ba.unsa.etf.rpr.dao.OdigranaKolaDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.TabelaDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.OdigranaKola;
+import ba.unsa.etf.rpr.domain.Tabela;
+
+import java.util.List;
+
 public class OdigranaKolaMenager {
 
     public void delete(int id)
     {
+        OdigranaKolaDaoSQLImpl odigranaKolaDaoSQL = new OdigranaKolaDaoSQLImpl();
+        List<OdigranaKola> odigranaKolas = odigranaKolaDaoSQL.getAll();
 
+        boolean brojac = false;
+
+        for(OdigranaKola odigranaKola: odigranaKolas)
+        {
+            if(odigranaKola.getId() == id) brojac = true;
+        }
+
+        if(brojac == false) throw new unosNijeIspravan("Tabela ne postoji");
+
+        odigranaKolaDaoSQL.delete(id);
     }
 
 }
