9e567a6b8877cb5587d3110ce115582d15851cf1
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-04 22:24:37 +0200

    rejtingManjiod0Test

--- src/test/java/ba/unsa/etf/rpr/UcesnikManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/UcesnikManagerTest.java
@@ -1,8 +1,19 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.UcesnikManager;
+import ba.unsa.etf.rpr.domain.Ucesnik;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UcesnikManagerTest {
 
+    @Test
+    void rejtingManjiOd0()
+    {
 
+
+
+
+    }
 }
