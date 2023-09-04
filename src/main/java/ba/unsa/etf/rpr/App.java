94c4f3a02420c2616bc6d02de763f093acef0b0a
AnesVrce <anes.vrce@chili-chips.xyz>
2023-09-05 01:59:12 +0200

    terminalni korisnicki interfejs

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -13,14 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        //UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
-        System.out.println("unesi ime i rejting: ");
         String izraz = new String(args[0]);
         Integer rejting = Integer.parseInt(args[1]);
 
         Ucesnik ucesnik = new Ucesnik();
         ucesnik.setBrojOsvojenihBodova(0.);
         ucesnik.setImeIPrezime(izraz);
+        ucesnik.setRejting(rejting);
 
+        UcesnikDaoSQLImpl ucesnikDaoSQL = new UcesnikDaoSQLImpl();
+        ucesnikDaoSQL.add(ucesnik);
     }
 }
